10e1aa3ad925e6d06b3b65a69ad7339f774b0bd9
Alex Grant <alex.grant@improbability.engineering>
2025-07-28 08:57:29 +0930

    token classification - untested

--- ass/tokenise.asm
+++ ass/tokenise.asm
@@ -8,14 +8,20 @@
 	DIRECTIVE = 3
 	MNEMONIC = 4
 	OPERAND = 5
+	EQUALS = 6
 
 
 	* = $c000
 
-	lda #<INPUT
+	lda #<INPUT		; Pointer to input data
 	sta ZP_PTR1
 	lda #>INPUT
 	sta ZP_PTR1+1
+
+	lda #<TOKEN		; Pointer to tokenised output
+	sta ZP_PTR0
+	lda #>TOKEN
+	sta ZP_PTR0+1
 
 .loop:
 	ldy #$0
@@ -29,13 +35,76 @@
 	ldy #$0			; skip comments
 	lda (ZP_PTR1),y
 	cmp #';'
-	beq .nextLine
+	bne .getTokenValue
+	jmp .nextLine
 
-	;; rest of token lexing goes here
-	jsr printString
-	lda #$d
-	jsr CHROUT
+.getTokenValue:
+	tax			; X holds first character
+	jsr getLexeme
 
+	;; Token classification
+	;;   X = first character
+	;;   A = last character
+.checkLabel:
+	cpx #':'
+	bne .checkDirective
+	lda #DIRECTIVE
+	sta (ZP_PTR0),y
+	jmp .nextToken
+	
+.checkDirective:
+	;; starts with a dot?
+	cmp #'.'
+	bne .checkEquals
+	lda #DIRECTIVE
+	sta (ZP_PTR0),y
+	jmp .nextToken
+
+.checkEquals:
+	;; ends with equals?
+	cpx #'='
+	bne .mnemonicOrOperand
+	lda #EQUALS
+	sta (ZP_PTR0),y
+	;; previous token is a SYMBOL
+	;; TODO need to add check to make sure there was a previous token
+	lda prevTokenValid
+	beq .checkEqualsDone
+	
+	lda #SYMBOL
+	sta (prevTokenType)
+
+.chackEqualsDone:
+	jmp .nextToken
+	
+.mnemonicOrOperand:
+	lda (prevTokenType)
+	cmp #MNEMONIC
+	bne .operand
+	lda #MNEMONIC
+	sta (ZP_PTR0),y
+	jmp .nextToken
+	
+.operand:
+	lda #OPERAND
+	sta (ZP_PTR0),y
+	jmp .nextToken
+
+	
+.nextToken:
+	lda ZP_PTR0
+	sta prevTokenType
+	lda ZP_PTR0+1
+	sta prevTokenType+1
+	lda #$01
+	sta prevTokenValid
+	
+	inc ZP_PTR0
+	bne .jmploop
+	inc ZP_PTR0+1
+.jmploop:
+	jmp .loop
+	
 .nextLine:
 	;; Skip to the next line (byte after the next null)
 	ldy #$0
@@ -67,3 +136,11 @@ INPUT:
 	string "	RTS"
 	string ".BYTE 0, 1, 2"
 	byte 0			; end of file
+
+prevTokenType:			; pointer to previous token TYPE 
+	byte 0, 0
+
+prevTokenValid:
+	byte 0			; 1 if there is a previous token 
+	
+TOKENS:	
