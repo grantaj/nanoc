53da8346712386ea8ca2698df27049bc1c773e19
Alex Grant <alex.grant@improbability.engineering>
2025-07-31 06:47:35 +0930

    display tokenised output

--- ass/tokenise.asm
+++ ass/tokenise.asm
@@ -13,6 +13,8 @@
 
 	* = $c000
 
+;;; test program
+	
 	lda #<INPUT		; Pointer to input data
 	sta ZP_PTR1
 	lda #>INPUT
@@ -23,10 +25,74 @@
 	lda #>TOKEN
 	sta ZP_PTR0+1
 
+	jsr .loop		; tokenise
+
+	;; output tokens
+	lda #<TOKEN		; Pointer to tokenised output
+	sta ZP_PTR0
+	lda #>TOKEN
+	sta ZP_PTR0+1
+
+.nextTokenChar:
+	lda (ZP_PTR0),Y
+	beq .tokenType        	; Null-terminator check
+	jsr CHROUT
+	inc ZP_PTR0
+	bne .nextTokenChar	; Keep printing if not page boundary	
+	inc ZP_PTR0+1     	; Handle page crossing
+	jmp .nextTokenChar
+
+.tokenType:
+	inc ZP_PTR0
+	bne .nextTokenChar	; Keep printing if not page boundary	
+	inc ZP_PTR0+1     	; Handle page crossing
+	lda #' '
+	jsr CHROUT
+.getTokenType:
+	lda (ZP_PTR0),y
+	cmp #$FF
+	bne .printTokenString
+	jmp .exit
+	
+.printTokenString:	
+	asl			; token strings are 10 bytes including NULL
+	asl			; multiply token type number by 10
+	asl 
+	clc
+	adc (ZP_PTR0),y
+	adc (ZP_PTR0),y
+
+	tay
+	lda #<.tokenStrings
+	sta ZP_PTR1
+	lda #>.tokenStrings
+	sta ZP_PTR1+1
+	jsr printString
+	lda #$0d
+	jsr CHROUT
+	jmp .nextTokenChar
+
+.exit:	
+	rts
+
+;;; tokeniser starts here
+
 .loop:
 	ldy #$0
 	lda (ZP_PTR1),y		; End of file marked with NULL
 	bne .nextToken
+
+	;; finished - mark end of tokens with $0,$ff
+	lda #$00
+	sta (ZP_PTR0),y
+
+	inc ZP_PTR0
+	bne .writeFF
+	inc ZP_PTR0+1
+.writeFF:	
+	lda #$FF
+	sta (ZP_PTR0),y
+	
 	rts
 	
 .nextToken:	
@@ -126,6 +192,21 @@
 	
 	include "printString.asm"
 	include "skipws.asm"
+	include "getLexeme.asm"
+
+;;; Token Strings Array
+.tokenStrings:
+	string "LABEL"
+	byte 0,0,0,0
+	string "SYMBOL"
+	byte 0,0
+	string "DIRECTIVE"
+	string "MNEMONIC"
+	byte 0
+	string "OPERAND"
+	byte 0,0,0
+	string "EQUALS"
+	byte 0,0,0,0
 
 ;;; A made up program to test tokenisation
 INPUT:
