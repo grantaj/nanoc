cad21e8608b3db485d6b2f5e701269e866f3995a
Alex Grant <alex.grant@improbability.engineering>
2026-08-31 09:51:45 +0930

    Isolate expression compile integration failure

--- tests/nanoc0-expr/expressions.c
+++ tests/nanoc0-expr/expressions.c
@@ -6,11 +6,4 @@ char byte_value = 255;
 int main()
 {
     int r00 = 0;
-    int r01 = 32767;
-    unsigned r02 = 32768;
-    unsigned r03 = 65535;
-    int r04 = byte_value;
-    int r05 = 65535 + 2;
-    int r06 = 0 - 1;
-    int r07 = 2 + 3 * 4;
 }
