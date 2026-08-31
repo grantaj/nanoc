b16c799d41bcc8a6f32eab00930bc1e6c802c3bf
Alex Grant <alex.grant@improbability.engineering>
2026-08-31 18:09:07 +0930

    Add native runtime compiler fixture

---
+++ nanoc0/test_runtime_compile.asm
@@ -0,0 +1,400 @@
+	include "../test.inc"
+
+RT_OUTPUT_LFN    = 3
+RT_OUTPUT_DEVICE = 9
+
+RT_FAIL_OPEN    = $20
+RT_FAIL_OUTPUT  = $21
+
+	* = $4000
+
+main:
+	lda #<$3800
+	sta bssBase
+	lda #>$3800
+	sta bssBase+1
+
+	lda #runtimeNameEnd-runtimeName
+	sta sourceNameLength
+	lda #<runtimeName
+	sta sourceName
+	lda #>runtimeName
+	sta sourceName+1
+	lda #$08
+	sta sourceDevice
+	lda #SOURCE_LFN_DEFAULT
+	sta sourceLfn
+	jsr open_source
+	bcc .openFailed
+
+	jsr rt_open_output
+	bcc .outputFailed
+	ldx #<rtHeader
+	ldy #>rtHeader
+	jsr emit_runtime_lines
+	bcc .outputFailed
+	jsr parse_translation_unit
+	bcc .compileFailed
+	jsr rt_close_output
+	lda #TEST_PASS
+	jmp .finish
+
+.openFailed:
+	lda #RT_FAIL_OPEN
+	jmp .finish
+.outputFailed:
+	jsr close_source
+	jsr rt_close_output
+	lda #RT_FAIL_OUTPUT
+	jmp .finish
+.compileFailed:
+	lda parserError
+	cmp #PARSE_SCANNER_ERROR
+	beq .scannerDiagnostic
+	cmp #PARSE_EXPRESSION_ERROR
+	bne .parserDiagnostic
+	lda expressionError
+	ora #$80
+	jmp .closeDiagnostic
+.scannerDiagnostic:
+	lda scannerError
+	ora #$20
+	jmp .closeDiagnostic
+.parserDiagnostic:
+	ora #$40
+.closeDiagnostic:
+	sta rtDiagnostic
+	jsr close_source
+	jsr rt_close_output
+	lda rtDiagnostic
+.finish:
+	sta TEST_RESULT
+.halt:
+	jmp .halt
+
+rt_open_output:
+	lda #outputNameEnd-outputName
+	ldx #<outputName
+	ldy #>outputName
+	jsr SETNAM
+	lda #RT_OUTPUT_LFN
+	ldx #RT_OUTPUT_DEVICE
+	ldy #RT_OUTPUT_LFN
+	jsr SETLFS
+	jsr OPEN
+	bcs .failed
+	lda #$01
+	sta rtOutputOpen
+	sta emitOutputEnabled
+	sec
+	rts
+.failed:
+	clc
+	rts
+
+rt_close_output:
+	lda #$00
+	sta emitOutputEnabled
+	jsr CLRCHN
+	lda #COMPILER_IO_NONE
+	sta compilerIecDirection
+	lda rtOutputOpen
+	beq .done
+	lda #RT_OUTPUT_LFN
+	jsr CLOSE
+	lda #$00
+	sta rtOutputOpen
+.done:
+	rts
+
+;;; ---------------------------------------------------------------------------
+;;; Direct declaration emission used by this one generated program
+;;; ---------------------------------------------------------------------------
+
+emit_persistent_symbol:
+	sta rtStorageKind
+	stx rtSavedSymbol
+	cmp #EMIT_STORAGE_BSS
+	beq .bss
+	cmp #EMIT_STORAGE_DATA
+	beq .data
+	ldx rtSavedSymbol
+	jsr emit_persistent_name
+	bcc .failed
+	lda #':'
+	jsr emit_output_byte
+	bcc .failed
+	jmp emit_newline
+.bss:
+	ldx rtSavedSymbol
+	jsr emit_persistent_name
+	bcc .failed
+	jmp rt_emit_bss_assignment
+.data:
+	ldx rtSavedSymbol
+	jsr emit_persistent_name
+	bcc .failed
+	lda #':'
+	jsr emit_output_byte
+	bcc .failed
+	jmp emit_newline
+.failed:
+	clc
+	rts
+
+emit_current_symbol:
+	stx rtSavedSymbol
+	jsr emit_current_name
+	bcc .failed
+	jmp rt_emit_bss_assignment
+.failed:
+	clc
+	rts
+
+emit_static_byte:
+	sta rtStaticByte
+	lda #rtBytePrefixEnd-rtBytePrefix
+	ldx #<rtBytePrefix
+	ldy #>rtBytePrefix
+	jsr emit_text
+	bcc .failed
+	lda rtStaticByte
+	jsr emit_hex_byte
+	bcc .failed
+	jmp emit_newline
+.failed:
+	clc
+	rts
+
+;;; Runtime-private storage must be allocated before the final BSS size is
+;;; spelled. The emitted support then sits before deferred string-literal data.
+emit_bss_boundaries:
+	jsr emit_runtime_support
+	bcc .failed
+	lda #rtBssEndPrefixEnd-rtBssEndPrefix
+	ldx #<rtBssEndPrefix
+	ldy #>rtBssEndPrefix
+	jsr emit_text
+	bcc .failed
+	lda bssOffset
+	sta emitWord
+	lda bssOffset+1
+	sta emitWord+1
+	jsr emit_hex_word
+	bcc .failed
+	jmp emit_newline
+.failed:
+	clc
+	rts
+
+rt_emit_bss_assignment:
+	lda #rtBssAssignEnd-rtBssAssign
+	ldx #<rtBssAssign
+	ldy #>rtBssAssign
+	jsr emit_text
+	bcc .failed
+	lda allocOffset
+	sta emitWord
+	lda allocOffset+1
+	sta emitWord+1
+	jsr emit_hex_word
+	bcc .failed
+	jmp emit_newline
+.failed:
+	clc
+	rts
+
+;;; ---------------------------------------------------------------------------
+;;; Generated test wrapper
+;;;
+;;; The wrapper is part of the generated ass source, so it calls the exact
+;;; support routines emitted below. Each public support entry is entered with D
+;;; set and must both behave correctly and return with D clear. main then performs
+;;; the higher-level file/EOF/error/smoke checks in ordinary Nano C.
+;;; ---------------------------------------------------------------------------
+
+rtHeader:
+	string "* = $0800"
+	string "NC_TMP = $fc"
+	string "NC_PTR = $fe"
+	string "NC_BSS = $3800"
+	string "__nc_test_entry:"
+	string "    sed"
+	string "    jsr __nc_init"
+	string "    jsr .check_d"
+	string "    bcc .fail_init"
+
+	string "    lda #$ff"
+	string "    sta NC_TMP"
+	string "    sta NC_TMP+1"
+	string "    lda #$02"
+	string "    ldx #$00"
+	string "    sed"
+	string "    jsr __nc_mul16"
+	string "    cmp #$fe"
+	string "    bne .fail_mul"
+	string "    cpx #$ff"
+	string "    bne .fail_mul"
+	string "    jsr .check_d"
+	string "    bcc .fail_mul"
+
+	string "    lda #<__nc_test_input"
+	string "    sta __c_io_open__v00"
+	string "    lda #>__nc_test_input"
+	string "    sta __c_io_open__v00+1"
+	string "    lda #$0a"
+	string "    sta __c_io_open__v01"
+	string "    lda #$00"
+	string "    sta __c_io_open__v01+1"
+	string "    sed"
+	string "    jsr __c_io_open"
+	string "    cpx #$00"
+	string "    bne .fail_open"
+	string "    cmp #$06"
+	string "    bcs .fail_open"
+	string "    sta __nc_test_handle"
+	string "    jsr .check_d"
+	string "    bcc .fail_open"
+
+	string "    lda __nc_test_handle"
+	string "    sta __c_io_read__v00"
+	string "    lda #$00"
+	string "    sta __c_io_read__v00+1"
+	string "    sed"
+	string "    jsr __c_io_read"
+	string "    cmp #'A'"
+	string "    bne .fail_read"
+	string "    cpx #$00"
+	string "    bne .fail_read"
+	string "    jsr .check_d"
+	string "    bcc .fail_read"
+
+	string "    lda __nc_test_handle"
+	string "    sta __c_io_close__v00"
+	string "    lda #$00"
+	string "    sta __c_io_close__v00+1"
+	string "    sed"
+	string "    jsr __c_io_close"
+	string "    cmp #$00"
+	string "    bne .fail_close"
+	string "    cpx #$00"
+	string "    bne .fail_close"
+	string "    jsr .check_d"
+	string "    bcc .fail_close"
+
+	string "    lda #<__nc_test_output"
+	string "    sta __c_io_create__v00"
+	string "    lda #>__nc_test_output"
+	string "    sta __c_io_create__v00+1"
+	string "    lda #$05"
+	string "    sta __c_io_create__v01"
+	string "    lda #$00"
+	string "    sta __c_io_create__v01+1"
+	string "    sed"
+	string "    jsr __c_io_create"
+	string "    cpx #$00"
+	string "    bne .fail_create"
+	string "    cmp #$06"
+	string "    bcs .fail_create"
+	string "    sta __nc_test_handle"
+	string "    jsr .check_d"
+	string "    bcc .fail_create"
+
+	string "    lda __nc_test_handle"
+	string "    sta __c_io_write__v00"
+	string "    lda #$00"
+	string "    sta __c_io_write__v00+1"
+	string "    lda #'Z'"
+	string "    sta __c_io_write__v01"
+	string "    lda #$00"
+	string "    sta __c_io_write__v01+1"
+	string "    sed"
+	string "    jsr __c_io_write"
+	string "    cmp #$00"
+	string "    bne .fail_write"
+	string "    cpx #$00"
+	string "    bne .fail_write"
+	string "    jsr .check_d"
+	string "    bcc .fail_write"
+
+	string "    lda __nc_test_handle"
+	string "    sta __c_io_close__v00"
+	string "    lda #$00"
+	string "    sta __c_io_close__v00+1"
+	string "    sed"
+	string "    jsr __c_io_close"
+	string "    cmp #$00"
+	string "    bne .fail_close"
+	string "    cpx #$00"
+	string "    bne .fail_close"
+	string "    jsr .check_d"
+	string "    bcc .fail_close"
+
+	string "    jsr __c_main"
+	string "    rts"
+	string ".check_d:"
+	string "    php"
+	string "    pla"
+	string "    and #$08"
+	string "    beq .d_clear"
+	string "    clc"
+	string "    rts"
+	string ".d_clear:"
+	string "    sec"
+	string "    rts"
+	string ".fail_init:"
+	string "    lda #$40"
+	string "    ldx #$00"
+	string "    rts"
+	string ".fail_mul:"
+	string "    lda #$41"
+	string "    ldx #$00"
+	string "    rts"
+	string ".fail_open:"
+	string "    lda #$42"
+	string "    ldx #$00"
+	string "    rts"
+	string ".fail_read:"
+	string "    lda #$43"
+	string "    ldx #$00"
+	string "    rts"
+	string ".fail_close:"
+	string "    lda #$44"
+	string "    ldx #$00"
+	string "    rts"
+	string ".fail_create:"
+	string "    lda #$45"
+	string "    ldx #$00"
+	string "    rts"
+	string ".fail_write:"
+	string "    lda #$46"
+	string "    ldx #$00"
+	string "    rts"
+	string "__nc_test_handle:"
+	string "    byte $00"
+	string "__nc_test_input:"
+	string "    byte 'R','U','N','T','I','M','E','.','I','N'"
+	string "__nc_test_output:"
+	string "    byte 'D','T','E','S','T'"
+	byte 0
+
+rtBytePrefix:		byte $09,'b','y','t','e',' ','$'
+rtBytePrefixEnd:
+rtBssAssign:		byte ' ','=',' ','N','C','_','B','S','S','+','$'
+rtBssAssignEnd:
+rtBssEndPrefix:	byte '_','_','n','c','_','b','s','s','_','e','n','d',' ','=',' ','N','C','_','B','S','S','+','$'
+rtBssEndPrefixEnd:
+
+runtimeName:	byte 'R','U','N','T','I','M','E','.','C'
+runtimeNameEnd:
+outputName:	byte 'R','T','O','U','T','.','A','S','M',',','S',',','W'
+outputNameEnd:
+
+rtOutputOpen:	byte 0
+rtDiagnostic:	byte 0
+rtStorageKind:	byte 0
+rtSavedSymbol:	byte 0
+rtStaticByte:	byte 0
+
+	include "declarations.asm"
+	include "runtime_codegen.asm"
