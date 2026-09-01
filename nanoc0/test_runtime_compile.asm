6497060d91c69fdf2453197f4dd6e94d9026e3b7
Alex Grant <alex.grant@improbability.engineering>
2026-09-02 06:04:08 +0930

    Use production output hooks in runtime acceptance test

--- nanoc0/test_runtime_compile.asm
+++ nanoc0/test_runtime_compile.asm
@@ -108,109 +108,16 @@ rt_close_output:
 	rts
 
 ;;; ---------------------------------------------------------------------------
-;;; Direct declaration emission used by this one generated program
-;;; ---------------------------------------------------------------------------
-
-emit_persistent_symbol:
-	sta rtStorageKind
-	stx rtSavedSymbol
-	cmp #EMIT_STORAGE_BSS
-	beq .bss
-	cmp #EMIT_STORAGE_DATA
-	beq .data
-	ldx rtSavedSymbol
-	jsr emit_persistent_name
-	bcc .failed
-	lda #':'
-	jsr emit_output_byte
-	bcc .failed
-	jmp emit_newline
-.bss:
-	ldx rtSavedSymbol
-	jsr emit_persistent_name
-	bcc .failed
-	jmp rt_emit_bss_assignment
-.data:
-	ldx rtSavedSymbol
-	jsr emit_persistent_name
-	bcc .failed
-	lda #':'
-	jsr emit_output_byte
-	bcc .failed
-	jmp emit_newline
-.failed:
-	clc
-	rts
-
-emit_current_symbol:
-	stx rtSavedSymbol
-	jsr emit_current_name
-	bcc .failed
-	jmp rt_emit_bss_assignment
-.failed:
-	clc
-	rts
-
-emit_static_byte:
-	sta rtStaticByte
-	lda #rtBytePrefixEnd-rtBytePrefix
-	ldx #<rtBytePrefix
-	ldy #>rtBytePrefix
-	jsr emit_text
-	bcc .failed
-	lda rtStaticByte
-	jsr emit_hex_byte
-	bcc .failed
-	jmp emit_newline
-.failed:
-	clc
-	rts
-
-;;; Runtime-private storage must be allocated before the final BSS size is
-;;; spelled. The emitted support then sits before deferred string-literal data.
-emit_bss_boundaries:
-	jsr emit_runtime_support
-	bcc .failed
-	lda #rtBssEndPrefixEnd-rtBssEndPrefix
-	ldx #<rtBssEndPrefix
-	ldy #>rtBssEndPrefix
-	jsr emit_text
-	bcc .failed
-	lda bssOffset
-	sta emitWord
-	lda bssOffset+1
-	sta emitWord+1
-	jsr emit_hex_word
-	bcc .failed
-	jmp emit_newline
-.failed:
-	clc
-	rts
-
-rt_emit_bss_assignment:
-	lda #rtBssAssignEnd-rtBssAssign
-	ldx #<rtBssAssign
-	ldy #>rtBssAssign
-	jsr emit_text
-	bcc .failed
-	lda allocOffset
-	sta emitWord
-	lda allocOffset+1
-	sta emitWord+1
-	jsr emit_hex_word
-	bcc .failed
-	jmp emit_newline
-.failed:
-	clc
-	rts
-
-;;; ---------------------------------------------------------------------------
 ;;; Generated test wrapper
 ;;;
 ;;; The wrapper sets decimal mode before one small C helper at a time. Each
 ;;; helper's first meaningful operation is the support/runtime call under test.
 ;;; Missing-file open and invalid-handle read deliberately cover error returns;
 ;;; the helper checks the result in Nano C and .check_result proves D=0.
+;;;
+;;; Declaration and runtime-support output below comes from program_output.asm,
+;;; exactly as it does in production nanoc0. Only this acceptance-test wrapper
+;;; is special.
 ;;; ---------------------------------------------------------------------------
 
 rtHeader:
@@ -321,13 +228,6 @@ rtHeader:
 	string "    rts"
 	byte 0
 
-rtBytePrefix:		byte $09,'b','y','t','e',' ','$'
-rtBytePrefixEnd:
-rtBssAssign:		byte ' ','=',' ','N','C','_','B','S','S','+','$'
-rtBssAssignEnd:
-rtBssEndPrefix:	byte '_','_','n','c','_','b','s','s','_','e','n','d',' ','=',' ','N','C','_','B','S','S','+','$'
-rtBssEndPrefixEnd:
-
 runtimeName:
 	byte 'T','E','S','T','S','/','N','A','N','O','C','0','-','R','U','N','T','I','M','E','/','R','U','N','T','I','M','E','.','C'
 runtimeNameEnd:
@@ -336,9 +236,7 @@ outputNameEnd:
 
 rtOutputOpen:	byte 0
 rtDiagnostic:	byte 0
-rtStorageKind:	byte 0
-rtSavedSymbol:	byte 0
-rtStaticByte:	byte 0
 
 	include "declarations.asm"
+	include "program_output.asm"
 	include "runtime_codegen.asm"
